1a1b041d2e87c834ba13b3bfcdecdee14898261c
nerobycrazy <nerobycrazy@yandex.ru>
2021-01-19 08:53:43 +0000

    Deploying to gh-pages from  @ ebdf8271b5f474166fabf45af9937c7f2824a50c 🚀

---
+++ precache-manifest.0ebdcf79cab525c05b50e742b51a1f92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3ff1918b061f62c7d3",
+    "url": "/dev/css/app.5c80f38f.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/dev/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "36e9055df68e15976085d0d057372673",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "8071d4a3c9ff45920970",
+    "url": "/dev/js/about.4750608a.js"
+  },
+  {
+    "revision": "2c3ff1918b061f62c7d3",
+    "url": "/dev/js/app.275dd95e.js"
+  },
+  {
+    "revision": "3369fcbe429b740ae591",
+    "url": "/dev/js/chunk-vendors.4aa47481.js"
+  },
+  {
+    "revision": "2d92139db899dbb0969d5d66af823675",
+    "url": "/dev/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dev/robots.txt"
+  }
+]);
